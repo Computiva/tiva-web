48725c22199178e79211782af06911275f135124
Angelo Nuffer <angelonuffer@gmail.com>
2016-03-04 22:32:16 -0300

    Add static dir.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-mkdir -p /srv/tiva/apps
+mkdir -p /srv/tiva/{apps,static}
 cp ./sbin/tivawebd /usr/sbin/tivawebd
 cp ./init.d/tivawebd /etc/init.d/tivawebd
 update-rc.d tivawebd defaults
